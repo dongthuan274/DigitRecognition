85cde8b66c73a2d1f46c390e824d14c951ba9e04
HaBao <habao26032006@gmail.com>
2024-11-26 17:10:49 +0700

    HaBao lam chunk flattening

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ def main():
     x_train, y_train = process.load_mnist("data/", kind="train")
     x_test, y_test = process.load_mnist("data/", kind="t10k")
 
+    train_flat, train_chunk, train_histogram = process.extract_features(x_train)
+    test_flat, test_chunk, test_histogram = process.extract_features(x_test)
+
     print(f"training images: {x_train.shape}")
     print(f"training labels: {y_train.shape}")
 
     print(f"test images: {x_test.shape}")
     print(f"test labels: {y_test.shape}")
+
+    print(f"training images after flattening by chunk: {train_chunk.shape}")
+    print(f"test images after flattening by chunk: {test_chunk.shape}")
 
 if __name__ == "__main__":
     main()
