2ac50f3f2629d11d9c4dc8571b89e59f9565201d
Thuan <24120146@student.hcmus.edu.vn>
2024-12-06 00:04:13 +0700

    update pkl file + predict label + graph test

--- process.py
+++ process.py
@@ -1,6 +1,8 @@
 import gzip
 import os
 import numpy as np
+import pickle
+import matplotlib.pyplot as plt
 
 IMG_SIZE = 28
 
@@ -87,6 +89,30 @@ def combine(vectorized_images, labels):
         combineList.append([vectorized_image, label])
     return combineList
 
+def gen_nearest_k_vectors(combineTest_images, combineTrain_images, output_file, k=1000):
+    if os.path.exists(output_file):
+        return
+    nearest_neighbors = []
+    for test_image, test_label in combineTest_images:
+        distances = []
+        for train_image, train_label in combineTrain_images:
+            dist = calcDist(test_image, train_image)
+            distances.append((dist, train_label))
+        distances.sort(key=lambda x: x[0])
+        nearest_labels = [train_label for ignore, train_label in distances[:k]]
+        nearest_neighbors.append(nearest_labels)
+    with open(output_file, 'wb') as f:
+        pickle.dump(nearest_neighbors, f)
+
+def load_pkl_file(file_name):
+    with open(file_name, 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+def predict_label_using_pkl(nearest_labels, k):
+    k_nearest = nearest_labels[:k]
+    return np.bincount(k_nearest).argmax()
+
 def predict_label(vectorized_image, combineTestImages, k):
     distances = []
     for i in combineTestImages:
@@ -97,3 +123,24 @@ def predict_label(vectorized_image, combineTestImages, k):
     nearest_labels = [label[1] for label in nearest_distances]
     most_common_label = max(nearest_labels, key=nearest_labels.count)
     return most_common_label
+
+def graph_accuracy_vs_k_pkl(combineTest_images, nearest_neighbors, k_values):
+    accuracies = []
+    true_labels = np.array([label for ignore, label in combineTest_images])
+    for k in k_values:
+        predictions = []
+        for nearest_labels in nearest_neighbors:
+            predicted_label = predict_label_using_pkl(nearest_labels, k)
+            predictions.append(predicted_label)
+        predictions = np.array(predictions)
+        correct_predictions = np.sum(predictions == true_labels)
+        accuracy = correct_predictions / len(combineTest_images)
+        accuracies.append(accuracy)
+    plt.figure(figsize=(10, 6))
+    plt.plot(k_values, accuracies, marker='o', color='b', label='Accuracy')
+    plt.xlabel("k (Number of Neighbors)")
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy vs k in k-Nearest Neighbors")
+    plt.grid()
+    plt.legend()
+    plt.show()
